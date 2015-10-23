f9ee3ac362419cf027f548e85dffc3cb648350e6
Marlon Casagranda <marlondiego407@gmail.com>
2015-10-23 21:29:19 -0200

    how it works and heroku

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret': 'ilovescotchyscotch',
-    //'database': 'mongodb://admin:admin@ds047812.mongolab.com:47812/jwtauth'
-		'database': 'mongodb://localhost:27017/jwtauth'
+    'database': 'mongodb://admin:admin@ds047812.mongolab.com:47812/jwtauth'
+		//'database': 'mongodb://localhost:27017/jwtauth'
 };
